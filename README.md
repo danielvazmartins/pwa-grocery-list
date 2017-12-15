e038ea373a1e5d706e2ba1d13c8dd6ab0d6830e1
Daniel Martins <daniel.martins@viewit.com.br>
2017-12-15 18:08:29 -0200

    fullscreen

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Lista de Compras utilizando recursos do PWA
 # Fontes
 https://www.schoolofnet.com/curso-progressive-web-apps/
 http://materializecss.com/
+https://whatwebcando.today/
+https://www.sitepoint.com/use-html5-full-screen-api/
--- js/main.js
+++ js/main.js
@@ -17,6 +17,41 @@ $(function(){
 		}		
 	});
 
+	$("#full-screen").click(function(e) {
+		e.preventDefault();
+
+		var fullElement = document.documentElement;
+
+		// Verifica se esta em fullScreen
+		if ( document.fullscreenElement || document.webkitFullscreenElement || 
+			 document.mozFullScreenElement || document.msFullscreenElement
+		) {
+			// Sai do modo fullScreen
+			if (document.exitFullscreen) {
+				document.exitFullscreen();
+			} else if (document.webkitExitFullscreen) {
+				document.webkitExitFullscreen();
+			} else if (document.mozCancelFullScreen) {
+				document.mozCancelFullScreen();
+			} else if (document.msExitFullscreen) {
+				document.msExitFullscreen();
+			}
+			$(".material-icons", this).text("fullscreen");
+		} else {
+			// Entra em fullScreen
+			if (fullElement.requestFullscreen) {
+				fullElement.requestFullscreen();
+			} else if (fullElement.webkitRequestFullscreen) {
+				fullElement.webkitRequestFullscreen();
+			} else if (fullElement.mozRequestFullScreen) {
+				fullElement.mozRequestFullScreen();
+			} else if (fullElement.msRequestFullscreen) {
+				fullElement.msRequestFullscreen();
+			}
+			$(".material-icons", this).text("fullscreen_exit");
+		}
+	});
+
 	function reloadProdutos() {
 		$("#lista").empty();
 		produtos.map(function(produto) {
